549af31fe0490dff1c4aef2f3e2b27e715097cd7
jtzhanghl <jtzhanghl@tedu.cn>
2022-12-07 10:53:08 +0800

    order模块

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
@@ -3,6 +3,7 @@ package cn.tedu.mall.seckill.mapper;
 import cn.tedu.mall.pojo.seckill.model.SeckillSpu;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -10,4 +11,13 @@ public interface SeckillSpuMapper {
 
     // 查询秒杀商品列表的方法
     List<SeckillSpu> findSeckillSpus();
+
+    // 根据指定时间,查询出正在进行秒杀的商品列表
+    List<SeckillSpu> findSeckillSpusByTime(LocalDateTime time);
+
+    // 根据spuId查询秒杀spu信息
+    SeckillSpu findSeckillSpuById(Long spuId);
+
+
+
 }
