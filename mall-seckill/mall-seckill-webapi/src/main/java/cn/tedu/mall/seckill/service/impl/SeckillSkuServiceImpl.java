bd0a36eab915f0607c99fb0e58d35984910335e1
jtzhanghl <jtzhanghl@tedu.cn>
2022-12-08 09:55:06 +0800

    order模块

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/service/impl/SeckillSkuServiceImpl.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/service/impl/SeckillSkuServiceImpl.java
@@ -1,5 +1,6 @@
 package cn.tedu.mall.seckill.service.impl;
 
+import cn.tedu.mall.pojo.seckill.model.SeckillSku;
 import cn.tedu.mall.pojo.seckill.vo.SeckillSkuVO;
 import cn.tedu.mall.product.service.seckill.IForSeckillSkuService;
 import cn.tedu.mall.seckill.mapper.SeckillSkuMapper;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,6 +28,15 @@ public class SeckillSkuServiceImpl implements ISeckillSkuService {
 
     @Override
     public List<SeckillSkuVO> listSeckillSkus(Long spuId) {
+        // 执行查询,根据spuId获取sku列表
+        List<SeckillSku> seckillSkus=skuMapper.findSeckillSkusBySpuId(spuId);
+        // 当前方法的返回值集合泛型为SeckillSkuVO
+        // 是既包含秒杀信息,又包含常规信息的对象
+        // 我们先实例化这个集合,以备后面循环遍历时向其添加元素
+        List<SeckillSkuVO> seckillSkuVOs=new ArrayList<>();
+
+
+
         return null;
     }
 }
