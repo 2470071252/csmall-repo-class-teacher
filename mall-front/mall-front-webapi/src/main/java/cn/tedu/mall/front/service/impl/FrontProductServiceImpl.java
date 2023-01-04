d53add93d9497afbaeab2cf958bc23cd91b52fe0
TEDU <83123095@qq.com>
2023-01-04 11:26:07 +0800

    商品列表

---
+++ mall-front/mall-front-webapi/src/main/java/cn/tedu/mall/front/service/impl/FrontProductServiceImpl.java
@@ -0,0 +1,49 @@
+package cn.tedu.mall.front.service.impl;
+
+import cn.tedu.mall.common.restful.JsonPage;
+import cn.tedu.mall.front.service.IFrontProductService;
+import cn.tedu.mall.pojo.product.vo.*;
+import cn.tedu.mall.product.service.front.IForFrontSpuService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class FrontProductServiceImpl implements IFrontProductService {
+
+    @DubboReference
+    private IForFrontSpuService dubboSpuService;
+
+    @Override
+    public JsonPage<SpuListItemVO> listSpuByCategoryId(Long categoryId, Integer page, Integer pageSize) {
+        // dubbo调用的方法是product模块编写的业务逻辑层方法
+        // 这个方法实际上完成了分页操作,我们只需要调用即可
+        JsonPage<SpuListItemVO> jsonPage=
+                dubboSpuService.listSpuByCategoryId(categoryId,page,pageSize);
+        // 别忘了返回jsonPage
+        return jsonPage;
+    }
+
+    @Override
+    public SpuStandardVO getFrontSpuById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<SkuStandardVO> getFrontSkusBySpuId(Long spuId) {
+        return null;
+    }
+
+    @Override
+    public SpuDetailStandardVO getSpuDetail(Long spuId) {
+        return null;
+    }
+
+    @Override
+    public List<AttributeStandardVO> getSpuAttributesBySpuId(Long spuId) {
+        return null;
+    }
+}
