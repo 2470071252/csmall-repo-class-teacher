7698e95d27028a536e97bf0b50a4cb6c7a596ed8
TEDU <83123095@qq.com>
2023-02-01 17:46:18 +0800

    订单列表

--- mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
+++ mall-search/mall-search-webapi/src/main/java/cn/tedu/mall/search/repository/SpuForElasticRepository.java
@@ -1,6 +1,7 @@
 package cn.tedu.mall.search.repository;
 
 import cn.tedu.mall.pojo.search.entity.SpuForElastic;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,18 @@ import org.springframework.stereotype.Repository;
 public interface SpuForElasticRepository extends
                             ElasticsearchRepository<SpuForElastic,Long> {
 
+    // 查询title字段中包含指定关键字的spu数据
+    Iterable<SpuForElastic> querySpuForElasticsByTitleMatches(String title);
+
+    @Query("\n" +
+            "    \"bool\": {\n" +
+            "      \"should\": [\n" +
+            "        { \"match\": { \"name\":  \"?0\" }},\n" +
+            "        { \"match\": { \"title\": \"?0\"}},\n" +
+            "        { \"match\": { \"description\": \"?0\"}},\n" +
+            "        { \"match\": { \"category_name\": \"?0\"}}\n" +
+            "      ]\n" +
+            "    }")
+    // 上面使用了指定搜索语句的方式来进行查询,下面的方法名就可以随意定义了
+    Iterable<SpuForElastic> querySearch(String keyword);
 }
--- mall-search/mall-search-webapi/src/test/java/cn/tedu/mall/search/test/SpuElasticTest.java
+++ mall-search/mall-search-webapi/src/test/java/cn/tedu/mall/search/test/SpuElasticTest.java
@@ -1,13 +1,39 @@
 package cn.tedu.mall.search.test;
 
 
+import cn.tedu.mall.pojo.search.entity.SpuForElastic;
+import cn.tedu.mall.search.repository.SpuForElasticRepository;
+import cn.tedu.mall.search.service.ISearchService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 // 下面注解必须加!!!!
 @SpringBootTest
 public class SpuElasticTest {
 
+    @Autowired
+    private ISearchService searchService;
+    @Test
+    void loadData(){
+        searchService.loadSpuByPage();
+        System.out.println("ok");
+    }
 
+    @Autowired
+    private SpuForElasticRepository repository;
+    @Test
+    void showData(){
+        Iterable<SpuForElastic> spus=repository.findAll();
+        spus.forEach(spu-> System.out.println(spu));
+    }
+
+    @Test
+    void showTitle(){
+        Iterable<SpuForElastic> spus=
+                repository.querySpuForElasticsByTitleMatches("手机");
+        spus.forEach(spu -> System.out.println(spu));
+    }
 
 
 }
