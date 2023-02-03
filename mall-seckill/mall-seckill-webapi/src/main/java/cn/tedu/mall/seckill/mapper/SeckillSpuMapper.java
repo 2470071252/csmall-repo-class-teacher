c8f4d879eace8442f239d8dc34d178ac692de763
TEDU <83123095@qq.com>
2023-02-03 11:59:49 +0800

    查询秒杀商品列表

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSpuMapper.java
@@ -3,6 +3,7 @@ package cn.tedu.mall.seckill.mapper;
 import cn.tedu.mall.pojo.seckill.model.SeckillSpu;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -10,6 +11,9 @@ public interface SeckillSpuMapper {
 
     // 查询秒杀商品列表的方法
     List<SeckillSpu> findSeckillSpus();
+
+    // 根据给定时间,查询正在进行秒杀的商品
+    List<SeckillSpu> findSeckillSpusByTime(LocalDateTime time);
 }
 
 
