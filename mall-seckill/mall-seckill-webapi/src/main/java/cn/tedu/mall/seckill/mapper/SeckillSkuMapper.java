55382294c5a7b538549868db70ba33196332181f
TEDU <83123095@qq.com>
2023-02-03 11:51:52 +0800

    查询秒杀商品列表

---
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSkuMapper.java
@@ -0,0 +1,12 @@
+package cn.tedu.mall.seckill.mapper;
+
+import cn.tedu.mall.pojo.seckill.model.SeckillSku;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SeckillSkuMapper {
+    // 根据spuId查询秒杀sku列表
+    List<SeckillSku> findSeckillSkusBySpuId(Long spuId);
+}
