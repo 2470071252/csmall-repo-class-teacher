6175fadb6c7102563403b0d86e43560f0be2d046
TEDU <83123095@qq.com>
2023-02-03 17:01:20 +0800

    查询秒杀商品列表

---
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/timer/config/QuartzConfig.java
@@ -0,0 +1,28 @@
+package cn.tedu.mall.seckill.timer.config;
+
+import cn.tedu.mall.seckill.timer.job.SeckillInitialJob;
+import org.quartz.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QuartzConfig {
+    @Bean
+    public JobDetail initJobDetail(){
+        return JobBuilder.newJob(SeckillInitialJob.class)
+                .withIdentity("initSeckill")
+                .storeDurably()
+                .build();
+    }
+    @Bean
+    public Trigger initTrigger(){
+        // 为了方便学习过程中的测试和运行的观察,cron表达式设置每分钟触发一次
+        CronScheduleBuilder cron=
+                CronScheduleBuilder.cronSchedule("0 0/1 * * * ?");
+        return TriggerBuilder.newTrigger()
+                .forJob(initJobDetail())
+                .withIdentity("initTrigger")
+                .withSchedule(cron)
+                .build();
+    }
+}
