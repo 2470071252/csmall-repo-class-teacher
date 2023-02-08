e2ed6568ec7414d13b093790a64a0d2894c0883e
TEDU <83123095@qq.com>
2023-02-08 11:55:56 +0800

    执行秒杀操作测试

--- mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSkuMapper.java
+++ mall-seckill/mall-seckill-webapi/src/main/java/cn/tedu/mall/seckill/mapper/SeckillSkuMapper.java
@@ -1,6 +1,8 @@
 package cn.tedu.mall.seckill.mapper;
 
 import cn.tedu.mall.pojo.seckill.model.SeckillSku;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +11,14 @@ import java.util.List;
 public interface SeckillSkuMapper {
     // 根据spuId查询秒杀sku列表
     List<SeckillSku> findSeckillSkusBySpuId(Long spuId);
+
+    // 根据skuId减少库存数的方法
+    int updateReduceStockBySkuId(@Param("skuId") Long skuId,
+                                 @Param("quantity") Integer quantity);
+
+
+
+
+
+
 }
